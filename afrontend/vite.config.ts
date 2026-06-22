c518aabca29a41fa4c502aa38b74269520875f8e
harshu-panchal <harshvardhanpanc145@gmail.com>
2026-06-23 03:25:07 +0530

    good night116

--- afrontend/vite.config.ts
+++ afrontend/vite.config.ts
@@ -68,7 +68,6 @@ export default defineConfig(({ mode }) => {
       },
       chunkSizeWarningLimit: 1000,
       sourcemap: false,
-      reportCompressedSize: false,
       minify: 'esbuild',
     },
   }
